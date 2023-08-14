18e265894ad583da035263232da32c89d37e3a36
Nicholas Bardy <nicholasbardy@gmail.com>
2023-08-14 04:03:21 -0700

    rot

--- src/trashPanda/experimental/test.ts
+++ src/trashPanda/experimental/test.ts
@@ -1,6 +1,35 @@
 import * as tf from "@tensorflow/tfjs";
-import { input, OptimizerConstructors, Tensor3D } from "@tensorflow/tfjs";
+import {
+  input,
+  OptimizerConstructors,
+  Tensor,
+  Tensor3D,
+} from "@tensorflow/tfjs";
 import { RotaryEmbedding } from "../embeddings/rotaryEmbeddings";
+
+export const downloadImage = (imageTensor: tf.Tensor3D, filename: string) => {
+  const canvas = document.createElement("canvas");
+  const [height, width, channels] = imageTensor.shape;
+  canvas.width = width;
+  canvas.height = height;
+  const ctx = canvas.getContext("2d")!;
+
+  const imageData = ctx.createImageData(width, height);
+  const data = imageTensor.dataSync();
+  for (let i = 0; i < height * width; ++i) {
+    const j = i * 4;
+
+    imageData.data[j + 0] = Math.round(255 * data[i * 3 + 0]);
+    imageData.data[j + 1] = Math.round(255 * data[i * 3 + 1]);
+    imageData.data[j + 2] = Math.round(255 * data[i * 3 + 2]);
+    imageData.data[j + 3] = 255;
+  }
+  ctx.putImageData(imageData, 0, 0);
+  const a = document.createElement("a");
+  a.href = canvas.toDataURL();
+  a.download = filename;
+  a.click();
+};
 
 // Test functions for printing image series to understand Rotary Embeddings
 //
@@ -8,7 +37,7 @@ import { RotaryEmbedding } from "../embeddings/rotaryEmbeddings";
 export const printMaxEmbeddingsImageSeries = () => {
   // Example Usage
   const dim = 8;
-  const rotaryEmbedding = new RotaryEmbedding(dim);
+  const rotaryEmbedding = new RotaryEmbedding({ dim });
   const tensor = tf.tensor2d([[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]]);
   const rotatedTensor = rotaryEmbedding.rotateQueriesOrKeys(tensor); // Shape: Bx8
 
@@ -46,7 +75,10 @@ export const printMaxEmbeddingsImageSeries = () => {
   ]);
 
   // Download the image
-  downloadImage(imageTensorRotatedReshaped, "rotaryEmbeddings.png");
+  downloadImage(
+    imageTensorRotatedReshaped as tf.Tensor3D,
+    "rotaryEmbeddings.png"
+  );
 
   // TODO: PrintSphericalPathEmbeddings
   // TODO: PrintSphericalPathEmbeddingsRotated
