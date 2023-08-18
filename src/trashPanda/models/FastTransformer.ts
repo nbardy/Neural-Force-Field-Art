881c896093fe629379942fedf44869848bc22ad7
Nicholas Bardy <nicholasbardy@gmail.com>
2023-08-17 20:05:48 -0700

    Update FastTransformer.ts

--- src/trashPanda/models/FastTransformer.ts
+++ src/trashPanda/models/FastTransformer.ts
@@ -1,154 +1,83 @@
-import * as tf from "@tensorflow/tfjs";
-import { LayersModel } from "@tensorflow/tfjs";
-import { RotaryEmbedding } from "../embeddings/rotaryEmbeddings";
+// experimental custom model with mobilevit blocks interspersed with swin
+//
+// inspired by papers that interleave conv and transformers for speed gains. 
+//
+// transforming mobilvit into a much deep versio, slows down mobilecit, but allows us to scale
+// compite with depth
+// todo: replace swin blocks with a faster windowed attention with global residual
+// first train with swin to get baseline.
+import * as tfjs from "@tfjs/core"
+import {SwinBlock} from "./swinTransformer.ts"
 
-import { MultiHeadAttention } from "../layers/multiheadattention";
-import { TrashPandaModel } from "../types";
+class FastTransformer {
+    constructor(image_size, dims, channels, num_classes, expansion = 4, kernel_size = 3, patch_size = [2, 2]) {
+        const [ih, iw] = image_size;
+        const [ph, pw] = patch_size;
+        if (ih % ph !== 0 || iw % pw !== 0) {
+            throw new Error("Image size should be divisible by patch size");
+        }
 
-class ResidualAttentionBlock {
-  attn: any; // BxCxWxH
-  ln_1: any; // BxCxWxH
-  mlp: any; // BxCxWxH
-  ln_2: any; // BxCxWxH
+        const L = [4, 4, 4];
+        const swinL = [4, 2, 2];
+   
+        this.conv1 = conv_nxn_bn(3, channels[0], 2);
 
-  rotaryEmbedding: RotaryEmbedding;
+        this.transformer = tfjs.seq([
+            new MV2Block(channels[0], channels[1], 1, expansion),
+            new MV2Block(channels[1], channels[2], 1, expansion),         
+            new MV2Block(channels[2], channels[3], 2, expansion),
+            new MV2Block(channels[2], channels[3], 1, expansion),
+            new MV2Block(channels[3], channels[4], 1, expansion),
+            new MobileViTBlock(dims[0], L[0], channels[5], kernel_size, patch_size, Math.floor(dims[0] * 2)),
+            new MV2Block(channels[5], channels[6], 1, expansion),
+            new MobileViTBlock(dims[1], L[1], channels[7], kernel_size, patch_size, Math.floor(dims[1] * 2)),
+            new MV2Block(channels[7], channels[8], 1, expansion),
+            new SwinBlock(swin_dims[0], swin_L[0], channels[9]),
+            new MV2Block(channels[9], channels[10], 1, expansion),
+            new MobileViTBlock(dims[2], L[2], channels[11], kernel_size, patch_size, Math.floor(dims[2] * 2)),
+            new SwinBlock(swin_dims[1], swin_L[1], channels[12]),
+            new MV2Block(channels[12], channels[13], 1, expansion),
+            new MV2Block(channels[13], channels[14], 1, expansion),
+            new SwinBlock(swin_dims[2], swin_L[2], channels[15]),
+            new MV2Block(channels[15], channels[16], 1, expansion),
+        ])
 
-  constructor({
-    modelDim: modelDim = 64,
-    attnHeads: attnHeads = 8,
-    attnHeadDim: attnHeadDim = 64,
-    dropout = 0,
-  }: {
-    modelDim?: number;
-    attnHeads?: number;
-    attnHeadDim?: number;
-    dropout?: number;
-  } = {}) {
-    this.attn = new MultiHeadAttention({
-      heads: attnHeads,
-      dim: modelDim,
-      dimHead: attnHeadDim,
-      dropout: dropout,
-    });
-
-    this.rotaryEmbedding = new RotaryEmbedding({
-      dim: modelDim,
-    });
-
-    this.ln_1 = tf.layers.layerNormalization({ axis: -1 });
-    this.mlp = tf.sequential({
-      layers: [
-        tf.layers.dense({ units: modelDim * 4, activation: "relu" }),
-        tf.layers.dense({ units: modelDim }),
-      ],
-    });
-    this.ln_2 = tf.layers.layerNormalization({ axis: -1 });
-  }
-
-  attention(x: tf.Tensor) {
-    const withRotaryEmbedding = this.rotaryEmbedding.rotateQueriesOrKeys(x);
-    return this.attn.apply([x, x, x]); // BxSxD
-  }
-
-  forward(x: tf.Tensor) {
-    let y = this.ln_1.apply(x);
-    y = this.attention(y);
-    x = tf.add(x, y); // BxSxD
-
-    y = this.ln_2.apply(x);
-    y = this.mlp.apply(y);
-    x = tf.add(x, y); // BxSxD
-
-    return x;
-  }
-}
-
-// Decoder only transformer used for GPT/CLIP
-export class FastTransformer implements TrashPandaModel {
-  layers; // BxSxD
-
-  constructor({
-    modelDim = 64,
-    attnHeads = 8,
-    attnHeadDim = 64,
-    dropout = 0,
-    depth: depth = 6,
-  }: {
-    modelDim?: number;
-    attnHeads?: number;
-    attnHeadDim?: number;
-    dropout?: number;
-    depth?: number;
-  } = {}) {
-
-    // TODO: Create windowed transformer that accepts the small tiles as a batch.
-    // Use rotary transformer
-    const compressedDim = modelDim/32;
-    self.global_window_transformer = RotaryTransformer({modelDim: compressedDim})
-    // cross attention to make final oatch
-    self.cross_attn_w1 = CrossAttention({dim: compressedDim});
-    self.cross_attn_w2 = CrossAttention({dim: compressedDim]);
-    self.seld_attn_w1 = SelfAttention({dim: compressedData]);
-
-    // TODO; modify the residual block to accept global token
-    // It should also compute windowed attention early with global residual windowed cross attention.
-    // and then later dds layers of global self attention, the cross attention with the last full layer reisdual.
-    
-    const layers = [];
-    const residual = x;
-
-    // W blocks are just repeated windowed sttention with global residual cross attention.
-    // self blocks are globak self sttention with a cross attention st the end of the stack.
-    // 
-
-    const layers = [ "8xW", "1xSelf", "8xW", "2xSelfCrossLastSelf", "8xW", "4xSelfCrossLast", "4xW", "2xSelfCross", "4xW", "1xSelfCross"]
-
-    const early_global_selfs = 4;
-    for(let i = 0; i < early_global_selfs) {
-        const block = new ResidualAttentionBlock({
-          modelDim,
-          attnHeads,
-          attnHeadDim,
-          dropout: dropout,
-        })
-      layers.push(block)
-     }
-
-    const early_windows = 8;
-    for(let i = 0; i < early_windows) {
-       const block = new WindowWithGlobalAttentionBlock({
-          modelDim,
-          attnHeads,
-          attnHeadDim,
-          contextDim: compressDim,
-          dropout: dropout,
-        })
-      layers.push(block)
+        this.conv2 = conv_1x1_bn(channels[channels.length - 2], channels[channels.length - 1]);
+        this.pool = tf.layers.averagePooling2d({ poolSize: [ih // 32, ih // 32], strides: 1 });
+        this.fc = tf.layers.dense({ units: num_classes, useBias: false });
     }
 
-    this.earlyWindows = seq;
+    call(input) {
+        let x = this.conv1.apply(input);
 
-    this.layers = layers;
-  }
+        x = this.mv2[0].call(x);
 
-  predict(x: tf.Tensor) {
-      // create 2d conv to downsample.
-      // flatten to 1d
-      // 1dconv to 1/4 the size.
-      // split to four windows.
-      // self attention across each as a batch.
-      // cross attention with the global attuon
-    let dx = this.downsample.apply(x)
-    dx = tf.flatten(dx)
-    dx = tf.1dconvshrinklayer.apply(x)
-    let wdx = tf.split(dx, 4, axis=1)
-    wdx = self.global_window_transformer.predict(wdx);
-    const [wdx1, wdx2, wdx3, wdx4] = wdx;
-    let global = self.cross_attn_w1(wdx1, wdx2) + self.cross_ttn_w2(wdx3, wdx4)
+        x = this.mv2[1].call(x);
+        x = this.mv2[2].call(x);
+        x = this.mv2[3].call(x); // Repeat
 
-    // todo eval stack as seq, but maintain a full global residual around the fast local layers and cross attention to it sfter the self sttentuon laywrs.
-    // for the fast windowed layers just drop them with the small windows fast, then fast cross attention on the windows siht the global resid.
-    
-    return this.layers.apply(x);
-  }
+        x = this.mv2[4].call(x);
+        x = this.mvit[0].call(x);
+
+        x = this.mv2[5].call(x);
+        x = this.mvit[1].call(x);
+
+        x = this.mv2[6].call(x);
+        x = this.mvit[2].call(x);
+        x = this.conv2.apply(x);
+
+        x = this.pool.apply(x);
+        x = x.reshape([x.shape[0], -1]); 
+        x = this.fc.apply(x);
+
+        return x;
+    }
+}
+
+function fastTransformerXXS() {
+    const dims = [128, 128, 128];
+    const dims = [64, 128, 128];
+    // const channels = [16, 16, 24, 24, 48, 48, 64, 64, 80, 80, 320];
+    const channels = [16, 16, 16, 16, 24, 24, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48];
+    return new MobileViT([256, 256], dims, channels, 1000, 2);
 }
